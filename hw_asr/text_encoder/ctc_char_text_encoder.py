aa21946fb1fb23d2371441c2fe91fe29d8894541
ArturGoldman <arturgoldman184@gmail.com>
2021-10-23 13:00:23 +0300

    Adding beam-search with shallow fusion

--- hw_asr/text_encoder/ctc_char_text_encoder.py
+++ hw_asr/text_encoder/ctc_char_text_encoder.py
@@ -6,8 +6,10 @@ from hw_asr.text_encoder.char_text_encoder import CharTextEncoder
 
 from ctcdecode import CTCBeamDecoder
 
+
 class CTCCharTextEncoder(CharTextEncoder):
     EMPTY_TOK = "^"
+    lm_model = './ASR-HW/lm.arpa'
 
     def __init__(self, alphabet: List[str]):
         super().__init__(alphabet)
@@ -33,11 +35,14 @@ class CTCCharTextEncoder(CharTextEncoder):
         ans_str = ans_str.replace(self.EMPTY_TOK, '')
         return ans_str
 
-    def ctc_beam_search(self, probs: torch.tensor, probs_length,
-                        beam_size: int = 50) -> List[Tuple[str, float]]:
+    def ctc_beam_search(self, probs: torch.tensor,
+                        beam_size: int = 100) -> List[Tuple[str, float]]:
         """
         Performs beam search and returns a list of pairs (hypothesis, hypothesis probability).
+
+        probs: [N_TIMESTEPS x N_VOCAB]
         """
+
         assert len(probs.shape) == 2
         char_length, voc_size = probs.shape
         assert voc_size == len(self.ind2char)
@@ -46,7 +51,7 @@ class CTCCharTextEncoder(CharTextEncoder):
             labels += self.ind2char[i]
         decoder = CTCBeamDecoder(
             labels,
-            model_path=None,
+            model_path=self.lm_model,
             alpha=0,
             beta=0,
             cutoff_top_n=40,
@@ -54,11 +59,10 @@ class CTCCharTextEncoder(CharTextEncoder):
             beam_width=beam_size,
             num_processes=4,
             blank_id=0,
-            log_probs_input=True
+            log_probs_input=False
         )
-        beam_results, beam_scores, timesteps, out_lens = decoder.decode(probs)
-        print(beam_results, beam_scores)
+        beam_results, beam_scores, timesteps, out_lens = decoder.decode(probs.unsqueeze(0))
         hypos = []
-        # TODO: your code here
-        raise NotImplementedError
+        for k in range(beam_size):
+            hypos.append((self.ctc_decode(beam_results[0][k][:out_lens[0][k]]), beam_scores[0][k].item()))
         return sorted(hypos, key=lambda x: x[1], reverse=True)
--- test.py
+++ test.py
@@ -1,23 +1,28 @@
 import argparse
 import json
-import os
 from pathlib import Path
 
 import torch
 from tqdm import tqdm
 
-import hw_asr.model as module_model
 from hw_asr.datasets.utils import get_dataloaders
 from hw_asr.text_encoder.ctc_char_text_encoder import CTCCharTextEncoder
+import hw_asr.model as module_model
+import hw_asr.loss as module_loss
+import hw_asr.metric as module_metric
 from hw_asr.trainer import Trainer
 from hw_asr.utils import ROOT_PATH
 from hw_asr.utils.parse_config import ConfigParser
+import torch.nn.functional as F
 
+DEFAULT_TEST_CONFIG_PATH = ROOT_PATH / "default_test_config.json"
 DEFAULT_CHECKPOINT_PATH = ROOT_PATH / "default_test_model" / "checkpoint.pth"
 
 
 def main(config, out_file):
     logger = config.get_logger("test")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # text_encoder
     text_encoder = CTCCharTextEncoder.get_simple_alphabet()
@@ -29,6 +34,13 @@ def main(config, out_file):
     model = config.init_obj(config["arch"], module_model, n_class=len(text_encoder))
     logger.info(model)
 
+    # get function handles of loss and metrics
+    loss_fn = config.init_obj(config["loss"], module_loss).to(device)
+    metric_fns = [
+        config.init_obj(metric_dict, module_metric, text_encoder=text_encoder)
+        for metric_dict in config["metrics"]
+    ]
+
     logger.info("Loading checkpoint: {} ...".format(config.resume))
     checkpoint = torch.load(config.resume)
     state_dict = checkpoint["state_dict"]
@@ -37,39 +49,35 @@ def main(config, out_file):
     model.load_state_dict(state_dict)
 
     # prepare model for testing
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     model.eval()
 
     results = []
 
     with torch.no_grad():
-        for batch_num, batch in enumerate(tqdm(dataloaders["test"])):
+        for i, batch in enumerate(tqdm(dataloaders["test"])):
             batch = Trainer.move_batch_to_device(batch, device)
-            output = model(**batch)
-            if type(output) is dict:
-                batch.update(output)
+            outputs = model(**batch)
+            if type(outputs) is dict:
+                batch.update(outputs)
             else:
-                batch["logits"] = output
-            batch["log_probs"] = torch.log_softmax(batch["logits"], dim=-1)
+                batch["logits"] = outputs
+
+            batch["log_probs"] = F.log_softmax(batch["logits"], dim=-1)
             batch["log_probs_length"] = model.transform_input_lengths(
                 batch["spectrogram_length"]
             )
             batch["probs"] = batch["log_probs"].exp().cpu()
             batch["argmax"] = batch["probs"].argmax(-1)
             for i in range(len(batch["text"])):
-                argmax = batch["argmax"][i]
-                argmax = argmax[:int(batch["log_probs_length"][i])]
-                results.append(
-                    {
-                        "ground_trurh": batch["text"][i],
-                        "pred_text_argmax": text_encoder.ctc_decode(argmax),
-                        "pred_text_beam_search": text_encoder.ctc_beam_search(
-                            batch["log_probs"], batch["log_probs_length"], beam_size=100
-                        )[:10],
-                    }
-                )
-    with Path(out_file).open("w") as f:
+                results.append({
+                    "ground_truth": batch["text"][i],
+                    "pred_text_argmax": text_encoder.ctc_decode(batch["argmax"][i][:int(batch["log_probs_length"][i])]),
+                    "pred_text_beam_search": text_encoder.ctc_beam_search(
+                        batch["probs"][i][:int(batch["log_probs_length"][i])], beam_size=100
+                    )[:10],
+                })
+    with Path(out_file).open('w') as f:
         json.dump(results, f, indent=2)
 
 
@@ -78,7 +86,7 @@ if __name__ == "__main__":
     args.add_argument(
         "-c",
         "--config",
-        default=None,
+        default=str(DEFAULT_TEST_CONFIG_PATH.absolute().resolve()),
         type=str,
         help="config file path (default: None)",
     )
@@ -99,7 +107,7 @@ if __name__ == "__main__":
     args.add_argument(
         "-o",
         "--output",
-        default="output.json",
+        default='output.json',
         type=str,
         help="File to write results (.json)",
     )
@@ -122,30 +130,16 @@ if __name__ == "__main__":
         "--jobs",
         default=1,
         type=int,
-        help="Number of workers for test dataloader",
+        help="Number of workers for test dataloader"
     )
 
+    config = ConfigParser.from_args(args)
+
     args = args.parse_args()
-
-    # set GPUs
-    if args.device is not None:
-        os.environ["CUDA_VISIBLE_DEVICES"] = args.device
-
-    # first, we need to obtain config with model parameters
-    # we assume it is located with checkpoint in the same folder
-    model_config = Path(args.resume).parent / "config.json"
-    with model_config.open() as f:
-        config = ConfigParser(json.load(f), resume=args.resume)
-
-    # update with addition configs from `args.config` if provided
-    if args.config is not None:
-        with Path(args.config).open() as f:
-            config.config.upadte(json.load(f))
-
-    # if `--test-data-folder` was provided, set it as a default test set
-    if args.test_data_folder is not None:
-        test_data_folder = Path(args.test_data_folder).absolute().resolve()
-        assert test_data_folder.exists()
+    if "test" not in config["data"]:
+        # this part brobably contains bugs
+        # i suggest you put test dataset in test config
+        test_data_folder = Path(args.test_data_folder)
         config.config["data"] = {
             "test": {
                 "batch_size": args.batch_size,
@@ -154,18 +148,11 @@ if __name__ == "__main__":
                     {
                         "type": "CustomDirAudioDataset",
                         "args": {
-                            "audio_dir": str(test_data_folder / "audio"),
-                            "transcription_dir": str(
-                                test_data_folder / "transcriptions"
-                            ),
-                        },
+                            "audio_dir": test_data_folder / "audio",
+                            "transcription_dir": test_data_folder / "transcriptions",
+                        }
                     }
-                ],
+                ]
             }
         }
-
-    assert config.config.get("data", {}).get("test", None) is not None
-    config["data"]["test"]["batch_size"] = args.batch_size
-    config["data"]["test"]["n_jobs"] = args.n_jobs
-
     main(config, args.output)
